d68db4fd9b2dfe0a1fbc4b0867538f040fd211ca
luxihua <youngseoma@naver.com>
2024-12-20 09:10:10 +0900

    bug : 모바일 내비게이션 js 코드 수정

--- nav.js
+++ nav.js
@@ -1,18 +1,4 @@
 window.onload = function () {
-    const languageToggleButton = document.querySelector('.language-toggle');
-    const languageDropdown = document.querySelector('.dropdown_lang');
-
-    languageToggleButton.addEventListener('click', function () {
-        // 드롭다운 메뉴의 표시 상태를 토글
-        languageDropdown.style.display = languageDropdown.style.display === 'block' ? 'none' : 'block';
-    });
-
-    // 다른 영역 클릭 시 드롭다운 메뉴 닫기
-    document.addEventListener('click', function (e) {
-        if (!languageToggleButton.contains(e.target) && !languageDropdown.contains(e.target)) {
-            languageDropdown.style.display = 'none';
-        }
-    });
 
     document.addEventListener("DOMContentLoaded", function () {
         const dropdownLinks = document.querySelectorAll(".dropdown_menu li a");
